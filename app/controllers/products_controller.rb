9abfdfe5d874635c0059bf0be1a4faa7d8086bdc
weiyueyan <xiaoguaishou0809@gmail.com>
2017-03-19 21:23:20 +0800

    implement carts index function

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -8,10 +8,11 @@ class ProductsController < ApplicationController
   end
 
   def add_to_cart
+    @product = Product.find(params[:id])
     current_cart.add_product_to_cart(@product)
     flash[:notice] = "成功加入购物车"
     redirect_to :back
-    #@product = Product.find(params[:id])
+
     #redirect_to :back, notice: "测试加入购物车"
   end
 end
